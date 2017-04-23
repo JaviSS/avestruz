4689449b66107a6a09c97019bcfbc4f1cec0e79b
JaviSS <javier.ssec@gmail.com>
2017-04-24 00:53:39 +0200

    ??

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AngubaseService} from './providers/angubase.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./css/chat.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  public usuarioEstaLogueado: boolean;
+
+  title = 'Avestruz!';
+
+  constructor(public _abService: AngubaseService, private _router: Router) {
+
+    // Con esto se comprueba asíncronamente si el usuario está logeado
+    // y es redirigido a la página de logueo si la stuación cambia
+    // Gracias firebase <3 fixme ._abService._angubaseService.
+
+    this._abService._angubaseService.auth.subscribe(
+      (auth) => {
+        if (auth == null) {
+          this.usuarioEstaLogueado = false;
+          console.log('Usuario no logueado, redirigiendo...');
+          this._router.navigate(['login']);
+
+        } else {
+          this.usuarioEstaLogueado = true;
+          console.log('Usuario logueado');
+          this._router.navigate(['']);
+        }
+      });
+  }
+
+  logout(){
+    this._abService.logout();
+  }
+
 }
+
