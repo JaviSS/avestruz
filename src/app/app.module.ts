0102b19d55544c3ede3ed7ec96f64d9239653b14
JaviSS <javier.ssec@gmail.com>
2017-04-23 22:40:48 +0200

    Boilerplate listo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
+import { AlertModule } from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
+import {AngularFireModule} from 'angularfire2';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCOVCKI69xJ8qf_aw_f_1R7L_4Jfl3-hD8',
+  authDomain: 'aves-truz.firebaseapp.com',
+  databaseURL: 'https://aves-truz.firebaseio.com',
+  projectId: 'aves-truz',
+  storageBucket: 'aves-truz.appspot.com',
+  messagingSenderId: '29801632317'
+};
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [AlertModule.forRoot(), BrowserModule, AngularFireModule.initializeApp(firebaseConfig)],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
